4dc950356df05771fa77490476ebdf93b6595a1b
Ralton Vaz <raltvaz@hotmail.com>
2017-07-31 00:09:41 -0400

    BT34 SuggestProjectileVelocity()

--- BattleTank/Source/BattleTank/Public/Tank.h
+++ BattleTank/Source/BattleTank/Public/Tank.h
@@ -38,6 +38,9 @@ private:
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
+	UPROPERTY(EditAnywhere, Category = Firing)
+	float LaunchSpeed = 100000; // TODO find sensible default
+
 
 	
 	
