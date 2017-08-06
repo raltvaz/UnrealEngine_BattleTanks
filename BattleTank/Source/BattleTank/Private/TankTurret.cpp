ac8bf17c45d4f5b6d726ae3c6b53c7088b128d92
Ralton Vaz <raltvaz@hotmail.com>
2017-08-06 19:19:04 -0400

    BT44 Turret Rotation Pt.2

--- BattleTank/Source/BattleTank/Private/TankTurret.cpp
+++ BattleTank/Source/BattleTank/Private/TankTurret.cpp
@@ -3,6 +3,16 @@
 #include "BattleTank.h"
 #include "TankTurret.h"
 
+void UTankTurret::Rotate(float RelativeSpeed) {
 
+	RelativeSpeed = FMath::Clamp<float>(RelativeSpeed, -1, +1);
+	auto RotationChange = RelativeSpeed * MaxDegreePerSecond * GetWorld()->DeltaTimeSeconds;
+	auto Rotation = RelativeRotation.Yaw + RotationChange; //
+
+	SetRelativeRotation(FRotator(0, Rotation, 0));
+
+	//auto Time = GetWorld()->GetTimeSeconds();
+	//UE_LOG(LogTemp, Warning, TEXT("%f: Barrel-Elevate() called at speed %f"), Time, RelativeSpeed);
+}
 
 
--- BattleTank/Source/BattleTank/Public/TankTurret.h
+++ BattleTank/Source/BattleTank/Public/TankTurret.h
@@ -13,7 +13,14 @@ class BATTLETANK_API UTankTurret : public UStaticMeshComponent
 {
 	GENERATED_BODY()
 	
-	
-	
+public:
+
+	// -1 is max downward speed, and +1 is max up movement
+	void Rotate(float RelativeSpeed);
+
+private:
+
+	UPROPERTY(EditAnywhere, Category = Setup)
+	float MaxDegreePerSecond = 25;
 	
 };
